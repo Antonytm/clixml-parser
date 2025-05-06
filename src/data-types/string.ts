975b35012b3eb9c2e9ff109af170cab63c099bc7
Antonytm <antontishchenko@gmail.com>
2025-05-07 01:41:49 +0300

    fix: fixing test build

--- src/data-types/string.ts
+++ src/data-types/string.ts
@@ -3,7 +3,7 @@ import { convertGeneric } from "./generic.js";
 export function convertString(input: any, property: string): any {
     const output: any = {};
     if (input[property] && typeof input[property] === "string") {
-        output[input["@_N1"]] = input["S"];
+        output[input["@_N"]] = input["S"];
         return output;
     }
     return convertGeneric(input, property);
